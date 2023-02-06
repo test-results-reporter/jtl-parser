1090868611c00c760382d5550729c2f9203781d5
Anudeep <asa.anudeep4@gmail.com>
2023-02-06 23:12:38 +0530

    fix

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
+const path = require('path');
 const fs = require("fs");
 const csv_json = require('csvjson');
 
 function parse(jtl_file_path) {
-  const text = fs.readFileSync(jtl_file_path, { encoding: 'utf8' });
+  const cwd = process.cwd();
+  const text = fs.readFileSync(path.join(cwd, jtl_file_path), { encoding: 'utf8' });
   const rows = csv_json.toObject(text, { delimiter: ',', quote: '"' });
   const aggregate_report = {};
+  const total_report = {
+    label: 'TOTAL',
+    samples: 0,
+    errors: 0,
+    elapsed_times: [],
+    max: Number.MIN_VALUE,
+    min: Number.MAX_VALUE,
+    total: 0,
+    received: 0,
+    sent: 0,
+  };
   let last_parent = '';
   for (const record of rows) {
     parseRecord(record);
@@ -14,10 +27,11 @@ function parse(jtl_file_path) {
     if (aggregate_report[record.label].parent) {
       last_parent = record.label;
     }
-    setCounts(aggregate_report, record, last_parent);
+    setCounts(aggregate_report, total_report, record, last_parent);
   }
   summarize(aggregate_report);
-  return aggregate_report;
+  setSummaryCounts(total_report);
+  return { transactions: aggregate_report, total: total_report };
 }
 
 function parseRecord(record) {
@@ -44,19 +58,41 @@ function setInitialAggregateReport(aggregate_report, record) {
   };
 }
 
-function setCounts(aggregate_report, record, last_parent) {
+function setCounts(aggregate_report, total_report, record, last_parent) {
   const transaction = aggregate_report[record.label];
+
   transaction.samples += 1;
+  total_report.samples += 1;
+
   if (record.responseCode >= 400) {
     transaction.errors += 1;
+    total_report.errors += 1;
   }
+
   transaction.max = Math.max(transaction.max, record.elapsed);
+  total_report.max = Math.max(total_report.max, record.elapsed);
+
   transaction.min = Math.min(transaction.min, record.elapsed);
+  total_report.min = Math.min(total_report.min, record.elapsed);
+
   transaction.total += record.elapsed;
+  total_report.total += record.elapsed;
+
   transaction.received += record.bytes;
+  total_report.received += record.bytes;
+
   transaction.sent += record.sentBytes;
+  total_report.sent += record.sentBytes;
+
   transaction.elapsed_times.push(record.elapsed);
+  total_report.elapsed_times.push(record.elapsed);
+
   transaction.end = record.timeStamp;
+  total_report.end = record.timeStamp;
+
+  if (!total_report.start) {
+    total_report.start = record.timeStamp;
+  }
 
   const parent_transaction = aggregate_report[last_parent];
   if (parent_transaction) {
@@ -68,16 +104,28 @@ function summarize(aggregate_report) {
   for (const key in aggregate_report) {
     if (Object.hasOwnProperty.call(aggregate_report, key)) {
       const transaction = aggregate_report[key];
-      transaction.tps = transaction.samples * 1000 / (transaction.end - transaction.start);
-      transaction.average = +((transaction.total / transaction.samples).toFixed(2));
-      const sorted_response_times = transaction.elapsed_times.sort((a, b) => b - a);
-      transaction.p50 = calculatePercentile(sorted_response_times, 50);
-      transaction.p90 = calculatePercentile(sorted_response_times, 90);
-      transaction.p95 = calculatePercentile(sorted_response_times, 95);
-      transaction.p99 = calculatePercentile(sorted_response_times, 99);
-      transaction.error_rate = +((transaction.errors / transaction.samples).toFixed(2));
+      setSummaryCounts(transaction);
     }
   }
+}
+
+function setSummaryCounts(transaction) {
+  transaction.tps = transaction.samples * 1000 / (transaction.end - transaction.start);
+  transaction.average = +((transaction.total / transaction.samples).toFixed(2));
+  const sorted_response_times = transaction.elapsed_times.sort((a, b) => b - a);
+  transaction.p50 = calculatePercentile(sorted_response_times, 50);
+  transaction.p90 = calculatePercentile(sorted_response_times, 90);
+  transaction.p95 = calculatePercentile(sorted_response_times, 95);
+  transaction.p99 = calculatePercentile(sorted_response_times, 99);
+  transaction.error_rate = +((transaction.errors / transaction.samples).toFixed(2));
+
+  const n = sorted_response_times.length;
+  if (n % 2 === 0) {
+    transaction.median = parseInt((sorted_response_times[n / 2] + sorted_response_times[(n / 2) + 1]) / 2);
+  } else {
+    transaction.median = sorted_response_times[(n + 1) / 2];
+  }
+
 }
 
 function calculatePercentile(sorted_response_times, percentile) {
@@ -86,6 +134,38 @@ function calculatePercentile(sorted_response_times, percentile) {
   return sorted_response_times.slice(0, xPercent).slice(-1)[0];
 };
 
+function aggregate(jtl_file_path) {
+  const { total, transactions } = parse(jtl_file_path);
+  const records = [];
+  for (const key in transactions) {
+    if (Object.hasOwnProperty.call(transactions, key)) {
+      const transaction = transactions[key];
+      records.push(getAggregateRecord(transaction));
+    }
+  }
+  records.push(getAggregateRecord(total));
+  return records;
+}
+
+function getAggregateRecord(transaction) {
+  return {
+    'Label': transaction.label,
+    '# Samples': transaction.samples,
+    'Throughput': transaction.tps,
+    'Average': transaction.average,
+    'Median': transaction.median,
+    '90% Line': transaction.p90,
+    '95% Line': transaction.p95,
+    '99% Line': transaction.p99,
+    'Min': transaction.min,
+    'Max': transaction.max,
+    'Error %': transaction.error_rate + '%',
+    'Sent KB/sec': transaction.sent,
+    'Received KB/sec': transaction.received,
+  }
+}
+
 module.exports = {
-  parse
+  parse,
+  aggregate
 };
--- tests/parse.spec.js
+++ tests/parse.spec.js
@@ -1,4 +1,4 @@
-const { parse } = require('../src');
+const { parse, aggregate } = require('../src');
 const { expect } = require('chai');
 
 describe('parse', () => {
@@ -6,59 +6,89 @@ describe('parse', () => {
   it('simple jtl file', () => {
     const result = parse('./tests/data/simple.jtl');
     expect(result).deep.equals({
-      call1: {
-        label: 'call1',
-        samples: 4,
-        errors: 1,
-        elapsed_times: [1170, 1131, 1023, 24],
-        max: 1170,
-        min: 24,
-        total: 3348,
-        received: 175,
-        sent: 175,
-        average: 837,
-        p50: 1131,
-        p90: 1170,
-        p95: 1170,
-        p99: 1170,
-        error_rate: 0.25,
-        start: 1448450968840,
-        end: 1448450969972,
-        tps: 3.5335689045936394,
-        parent: false
+      transactions: {
+        call1: {
+          label: 'call1',
+          samples: 4,
+          errors: 1,
+          elapsed_times: [1170, 1131, 1023, 24],
+          max: 1170,
+          min: 24,
+          total: 3348,
+          received: 175,
+          sent: 175,
+          average: 837,
+          p50: 1131,
+          p90: 1170,
+          p95: 1170,
+          p99: 1170,
+          error_rate: 0.25,
+          start: 1448450968840,
+          end: 1448450969972,
+          tps: 3.5335689045936394,
+          parent: false,
+          median: 523,
+        },
+        call2: {
+          label: 'call2',
+          samples: 4,
+          errors: 0,
+          elapsed_times: [2200, 1907, 938, 833],
+          max: 2200,
+          min: 833,
+          total: 5878,
+          received: 180,
+          sent: 180,
+          average: 1469.5,
+          p50: 1907,
+          p90: 2200,
+          p95: 2200,
+          p99: 2200,
+          error_rate: 0,
+          start: 1448450969091,
+          end: 1448450969203,
+          tps: 35.714285714285715,
+          parent: false,
+          median: 885,
+        }
       },
-      call2: {
-        label: 'call2',
-        samples: 4,
-        errors: 0,
-        elapsed_times: [2200, 1907, 938, 833],
+      total: {
+        samples: 8,
+        errors: 1,
+        elapsed_times: [
+          2200, 1907, 1170,
+          1131, 1023,  938,
+           833,   24
+        ],
         max: 2200,
-        min: 833,
-        total: 5878,
-        received: 180,
-        sent: 180,
-        average: 1469.5,
-        p50: 1907,
+        min: 24,
+        total: 9226,
+        received: 355,
+        sent: 355,
+        end: 1448450969972,
+        start: 1448450968840,
+        tps: 7.067137809187279,
+        average: 1153.25,
+        p50: 1131,
         p90: 2200,
         p95: 2200,
         p99: 2200,
-        error_rate: 0,
-        start: 1448450969091,
-        end: 1448450969203,
-        tps: 35.714285714285715,
-        parent: false
+        error_rate: 0.13,
+        median: 980,
+        label: "TOTAL"
       }
     });
   });
 
   it('complex jtl file', () => {
     const result = parse('./tests/data/complex.jtl');
-    expect(result['S01_Create_Booking_T01_Application_Launch']).deep.equals({
+    expect(result.transactions['S01_Create_Booking_T01_Application_Launch']).deep.equals({
       label: 'S01_Create_Booking_T01_Application_Launch',
       samples: 5,
       errors: 0,
       elapsed_times: [1010, 521, 465, 358, 356],
       max: 1010,
+      median: 358,
       min: 356,
       total: 2710,
       received: 230278,
@@ -74,6 +104,61 @@ describe('parse', () => {
       p99: 1010,
       error_rate: 0
     });
+  });
+
+});
+
+describe('aggregate', () => {
+
+  it('simple jtl file', () => {
+    const result = aggregate('./tests/data/simple.jtl');
+    expect(result).deep.equals([
+      {
+        Label: 'call1',
+        '# Samples': 4,
+        Throughput: 3.5335689045936394,
+        Average: 837,
+        Median: 523,
+        '90% Line': 1170,
+        '95% Line': 1170,
+        '99% Line': 1170,
+        Min: 24,
+        Max: 1170,
+        'Error %': '0.25%',
+        'Sent KB/sec': 175,
+        'Received KB/sec': 175
+      },
+      {
+        Label: 'call2',
+        '# Samples': 4,
+        Throughput: 35.714285714285715,
+        Average: 1469.5,
+        Median: 885,
+        '90% Line': 2200,
+        '95% Line': 2200,
+        '99% Line': 2200,
+        Min: 833,
+        Max: 2200,
+        'Error %': '0%',
+        'Sent KB/sec': 180,
+        'Received KB/sec': 180
+      },
+      {
+        Label: 'TOTAL',
+        '# Samples': 8,
+        Throughput: 7.067137809187279,
+        Average: 1153.25,
+        Median: 980,
+        '90% Line': 2200,
+        '95% Line': 2200,
+        '99% Line': 2200,
+        Min: 24,
+        Max: 2200,
+        'Error %': '0.13%',
+        'Sent KB/sec': 355,
+        'Received KB/sec': 355
+      }
+    ]);
   });
 
 });
